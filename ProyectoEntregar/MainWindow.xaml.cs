3f1a57e49c18513603b11e1df1d724c01319fe33
adrichxng <chang.vk4@gmail,com>
2023-02-01 00:28:39 +0100

    update

--- ProyectoEntregar/MainWindow.xaml.cs
+++ ProyectoEntregar/MainWindow.xaml.cs
@@ -35,75 +35,23 @@ namespace ProyectoEntregar
 
         private void initialize()
         {
-            Title = "Título de la Interfaz";
-
-
-            botones.Add(bt_habP);
-            var banio = new Button { Content = "Baño" };
-            botones.Add(banio);
-            cuartos.Add(bt_habP.Content.ToString());
-            cuartos.Add(banio.Content.ToString());
-
-            var botonMas = new Button { Content = "+" };
-            botonMas.Click += botonMas_Click;
-
-            barraSuperior.Children.Add(bt_habP);
-            barraSuperior.Children.Add(banio);
-            barraSuperior.Children.Add(botonMas);
-
-            var panelPrincipal = new StackPanel();
-            panelPrincipal.Children.Add(barraSuperior);
-
-            Content = panelPrincipal;
         }
 
         private void botonMas_Click(object sender, RoutedEventArgs e)
         {
-            var nueva = new AddRoom(this);
-            nueva.ShowDialog();
-
-            if (nueva.seleccionado.Equals("cocina", StringComparison.OrdinalIgnoreCase))
-            {
-                if (YaExiste(nueva))
-                {
-                    MessageBox.Show("No puedes añadir eso", "ERROR", MessageBoxButton.OK, MessageBoxImage.Warning);
-                }
-                else
-                {
-                    cuartos.Add(cocina.Content.ToString());
-                    botones.Add(cocina);
-                    barraSuperior.Children.Add(cocina);
-                }
-            }
-            else if (nueva.seleccionado.Equals("salon", StringComparison.OrdinalIgnoreCase))
-            {
-                if (YaExiste(nueva))
-                {
-                    MessageBox.Show("No puedes añadir eso", "ERROR", MessageBoxButton.OK, MessageBoxImage.Warning);
-                }
-                else
-                {
-                    cuartos.Add(salon.Content.ToString());
-                    botones.Add(salon);
-                    barraSuperior.Children.Add(salon);
-                }
-            }
 
         }
 
-        private bool YaExiste(AddRoom nueva)
-        {
-            return cuartos.Contains(nueva.seleccionado);
-        }
+        
 
         private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
 
-        private void Button_Click()
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            
         }
     }
 }
